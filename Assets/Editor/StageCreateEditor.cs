daf9d79bb7b071af9d9561e5b217ccd85014eb31
mikanguma <sinrei20012003@gmail.com>
2024-03-05 23:12:56 +0900

    ステージ名を入力できるように改良

--- Assets/Editor/StageCreateEditor.cs
+++ Assets/Editor/StageCreateEditor.cs
@@ -10,15 +10,21 @@ using UnityEngine;
 [CustomEditor(typeof(StageArrayDataForTilemap))]
 public class StageCreateEditor : Editor
 {
+    [Tooltip("（必ずナンバリングすること）例：Stage1")]
+    private string _stageName = default;
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
+
+        _stageName = EditorGUILayout.TextField("ステージ名", _stageName);
 
         StageArrayDataForTilemap stageArrayData = target as StageArrayDataForTilemap;
 
         if(GUILayout.Button("ステージ情報を出力"))
         {
             stageArrayData.Initialize();
+
+            Debug.Log(_stageName);
 
             //配列を出力する
             Debug.Log("Field--------------------------------------------");
