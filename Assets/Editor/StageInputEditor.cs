b671a734d900b2f759824b11798a1c97d8093f4a
s20227052 <20227052-kobayashishin@hcs.ac.jp>
2024-03-11 14:50:07 +0900

    ステージ名の入力をドロップダウンに移行、予定した作業が終了

--- Assets/Editor/StageInputEditor.cs
+++ Assets/Editor/StageInputEditor.cs
@@ -13,8 +13,12 @@ using System.IO;
 
 public class StageInputEditor : StageCreateEditor
 {
+    // 除外するファイルの拡張子
+    private const string EXCLUSION_EXTENSION = ".meta";
+    // 削除する拡張子
+    private const string DELETE_EXTENSION = ".csv";
+    // 検索するフォルダー
     private const string STAGE_DATA_FOLDE = "Assets/StageData";
-    private List<string> _stageNameList = new List<string>();
     private int _stageIndex = 0;
 
     [MenuItem("Stage/StageInput", false, 1)]
@@ -26,49 +30,75 @@ public class StageInputEditor : StageCreateEditor
 
     protected override void OnGUI()
     {
-        base.InputProperty();
+        // レイアウトを調整するために隙間を作る
+        GUILayout.Space(30f);
 
+        // 指定したフォルダーのファイルを全て取得する
         var fileNames = Directory.GetFiles(STAGE_DATA_FOLDE);
 
-        /*for (int i = 0; i < fileNames.Length; i++)
-        {
-            Debug.Log(fileNames[i]);
-        }*/
+        List<string> stageFileList = new List<string>();
 
-        string[] stageNames = _stageNameList.ToArray();
+        for (int i = 0; i < fileNames.Length; i++)
+        {
+            // 指定した拡張子のファイルを除外する
+            if (!fileNames[i].Contains(EXCLUSION_EXTENSION))
+            {
+                stageFileList.Add(Path.GetFileName(fileNames[i]));
+            }
+        }
+
+        // ドロップダウン表示用に配列に変換する
+        string[] stageFiles = stageFileList.ToArray();
 
         _stageIndex = EditorGUILayout.Popup(
             label: new GUIContent("生成ステージ名"),
             selectedIndex: _stageIndex,
-            displayedOptions: stageNames
+            displayedOptions: stageFiles
             );
+
+        base.InputProperty();
 
         if (GUILayout.Button("ステージを生成"))
         {
             // タイルをすべて削除する
             base._targetTilemap.ClearAllTiles();
 
-            TextAsset inputData = Addressables.LoadAssetAsync<TextAsset>(stageNames[_stageIndex]).WaitForCompletion();
-            string textData =inputData.text;
+            // ファイルから拡張子を取り除く
+            string stageName = stageFiles[_stageIndex].Replace(DELETE_EXTENSION, "");
 
-            string[] data = textData.Split(new char[] { '\r', '\n' }, System.StringSplitOptions.RemoveEmptyEntries);
-            base._verticalMaxSize = data.Length;
+            // CSVファイルを読み込む
+            TextAsset inputData = Addressables.LoadAssetAsync<TextAsset>(stageName).WaitForCompletion();
+            string textData = inputData.text;
 
-            string[] lineCheck = data[0].Split(",", System.StringSplitOptions.RemoveEmptyEntries);
-            base._horizontalMaxSize = lineCheck.Length;
+            // 行単位に分割する
+            // そのままだと改行も入るため、RemoveEmptyEntriesで削除する
+            string[] row = textData.Split(new char[] { '\r', '\n' }, System.StringSplitOptions.RemoveEmptyEntries);
+            // 行数を取得する
+            base._verticalMaxSize = row.Length;
 
-            for (int i = 0; i < base._verticalMaxSize; i++)
+            // 列単位に分割する
+            string[] columnCheck = row[0].Split(",");
+            // 列数を取得する
+            base._horizontalMaxSize = columnCheck.Length;
+
+            for (int y = 0; y < base._verticalMaxSize; y++)
             {
-                string[] line = data[i].Split(",", System.StringSplitOptions.RemoveEmptyEntries);
+                string[] column = row[y].Split(",");
 
-                for (int j = 0; j < base._horizontalMaxSize; j++)
+                for (int x = 0; x < base._horizontalMaxSize; x++)
                 {
-                    SetTile(int.Parse(line[j]), j, i);
+                    SetTile(int.Parse(column[x]), x, y);
                 }
             }
         }
     }
 
+    /// <summary>
+    /// 指定したタイルマップ座標に対象のタイルを設置する
+    /// </summary>
+    /// <param name="number"></param>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
     private void SetTile(int number, int x, int y)
     {
         Vector3Int setPos = new Vector3Int(x, -y);
