f08d6987114944e941f1a91326cc56e066fd8065
mikanguma <sinrei20012003@gmail.com>
2024-03-11 22:31:45 +0900

    アタッチするタイルの部分をリストにする

--- Assets/Editor/StageCreateEditor.cs
+++ Assets/Editor/StageCreateEditor.cs
@@ -4,9 +4,11 @@
 // 作成日:2024/03/09
 // 作成者:小林慎
 // ---------------------------------------------------------
-using UnityEngine;
 using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
 using UnityEngine.Tilemaps;
+using System.Collections.Generic;
 
 public abstract class StageCreateEditor : EditorWindow
 {
@@ -28,6 +30,11 @@ public abstract class StageCreateEditor : EditorWindow
     [Header("対象タイル４（値：４）")]
     [SerializeField]
     protected TileBase _targetTile4 = default;
+
+    [Header("対象タイル")]
+    [SerializeField]
+    protected List<TileBase> _targetTileList = new List<TileBase>();
+    private ReorderableList _reorderableList = default;
 
     // ステージの横の最大サイズ
     protected int _horizontalMaxSize = default;
@@ -56,6 +63,28 @@ public abstract class StageCreateEditor : EditorWindow
         EditorGUILayout.PropertyField(serializedObject.FindProperty($"{nameof(_targetTile2)}"));
         EditorGUILayout.PropertyField(serializedObject.FindProperty($"{nameof(_targetTile3)}"));
         EditorGUILayout.PropertyField(serializedObject.FindProperty($"{nameof(_targetTile4)}"));
+
+        SerializedProperty list = serializedObject.FindProperty($"{nameof(_targetTileList)}");
+
+        if(_reorderableList == null)
+        {
+            _reorderableList = new ReorderableList(serializedObject, list);
+
+            // タイトル描画時のコールバック
+            // 上書きしてEditorGUIを使えばタイトル部分を自由にレイアウトできる
+            _reorderableList.drawHeaderCallback = rect => EditorGUI.LabelField(rect, "対象タイル");
+
+            // 要素の描画時のコールバック
+            // 上書きしてEditorGUIを使えば自由にレイアウトできる
+            _reorderableList.drawElementCallback = (rect, index, isActive, isFocused) =>
+            {
+                var elementProperty = list.GetArrayElementAtIndex(index);
+                rect.height = EditorGUIUtility.singleLineHeight;
+                EditorGUI.PropertyField(rect, elementProperty, new GUIContent("対象タイル" + (index + 1) + "（値：" + (index + 1) + "）"));
+            };
+        }
+
+        _reorderableList.DoLayoutList();
 
         // 入力された値を反映させる
         serializedObject.ApplyModifiedProperties();
